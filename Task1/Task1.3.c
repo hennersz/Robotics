661b6607bbb8e819516d4f51a2053ec57fe07ede
Axel Goetz <axel.goez@hotmail.com>
2015-01-16 17:30:45 +0000

    Revert 21309f2..59d686b
    This rolls back to commit 21309f24055631bfbe16675cbbcb90404b32ffee.

--- Task1/Task1.3.c
+++ Task1/Task1.3.c
@@ -19,10 +19,9 @@ void turn (char c, float angle, float speed)
 	get_motor_encoders(leftcount, rightcount);    
 	int initialLeft = *leftcount;	//store initial values
 	int initialRight = *rightcount;
-	float ratio = 2.33333333;
+	float ratio = 214/90.0;
 	printf("Ratio = %f\n", ratio);
 	float encoder = ratio*angle;
-	float percentageLeft, percentageRight;
 	if (c == 'L')
 	{
 		while (1)
@@ -30,20 +29,10 @@ void turn (char c, float angle, float speed)
 			get_motor_encoders(leftcount, rightcount);
 			differenceLeft = abs(*leftcount - initialLeft);
 			differenceRight = abs(*rightcount - initialRight);
-			percentageLeft = differenceLeft/encoder;
-			percentageRight = differenceRight/encoder;
-			printf("percentageLeft = %f, percentageRight = %f\n", percentageLeft, percentageRight);
-			if((percentageRight>0.5)&&(percentageLeft>0.5))
-			{
-				speedLeft = (((encoder - differenceLeft)/encoder)*speed) + 1;
-				speedRight = (((encoder - differenceRight)/encoder)*speed) + 1;
-			}
-			else
-			{
-				speedLeft = speedRight = speed;
-			}
+			speedLeft = (((encoder - differenceLeft)/encoder)*speed) + 1;
+			speedRight = (((encoder - differenceRight)/encoder)*speed) + 1;
 
-			//printf("T\t%i\t%i\t%f\t%f\n", differenceLeft, differenceRight, speedLeft, speedRight);
+			printf("T\t%i\t%i\t%f\t%f\n", differenceLeft, differenceRight, speedLeft, speedRight);
 			if (differenceLeft == (int)encoder && differenceRight == (int)encoder)
 				break;
 			else if (differenceLeft > encoder && differenceRight > encoder)
@@ -58,17 +47,8 @@ void turn (char c, float angle, float speed)
 			get_motor_encoders(leftcount, rightcount);
 			differenceLeft = abs(*leftcount - initialLeft);
 			differenceRight = abs(*rightcount - initialRight);
-			percentageLeft = ((encoder - differenceLeft)/encoder);
-			percentageRight = ((encoder - differenceRight)/encoder);
-			if((percentageRight>0.7)&&(percentageLeft>0.7))
-			{
-				speedLeft = (((encoder - differenceLeft)/encoder)*speed) + 1;
-				speedRight = (((encoder - differenceRight)/encoder)*speed) + 1;
-			}
-			else
-			{
-				speedLeft = speedRight = speed;
-			}
+			speedLeft = (((encoder - differenceLeft)/encoder)*speed) + 1;
+			speedRight = (((encoder - differenceRight)/encoder)*speed) + 1;
 
 			printf("T\t%i\t%i\t%f\t%f\n", differenceLeft, differenceRight, speedLeft, speedRight);
 			if (differenceLeft == (int)encoder && differenceRight == (int)encoder)
@@ -80,40 +60,31 @@ void turn (char c, float angle, float speed)
 		}
 }
 
-void straightLine(float distance, float speed)
+void straightLine(int x, float speed)
 {
 	int *leftcount, *rightcount;
 	float speedLeft, speedRight;
 	int differenceLeft = 0;
 	int differenceRight = 0;
-	float distanceTarget = distance*1194.0;
+
 	int test = 5;
 	leftcount = rightcount = &test;
 	get_motor_encoders(leftcount, rightcount);    //find initial values
 	int initialLeft = *leftcount;
 	int initialRight = *rightcount;
-	float percentageLeft, percentageRight;
+
 	while(1)
 	{
 		get_motor_encoders(leftcount, rightcount);
 		differenceLeft = abs(*leftcount - initialLeft);
 		differenceRight = abs(*rightcount - initialRight);
-		percentageLeft = ((distanceTarget - differenceLeft)/distanceTarget);
-		percentageRight = ((distanceTarget - differenceRight)/distanceTarget);
-		if(percentageRight>0.9&&percentageLeft>0.9)
-		{
-			speedLeft = (((distanceTarget - differenceLeft)/distanceTarget)*speed) + 1;
-			speedRight = (((distanceTarget - differenceRight)/distanceTarget)*speed) + 1;
-		}
-		else
-		{
-			speedLeft = speedRight = speed;
-		}
+		speedLeft = (((float)(x - differenceLeft)/x)*speed) + 1;
+		speedRight = (((float)(x - differenceRight)/x)*speed) + 1;
 		log_trail();
-		printf("L\t%i\t%i\t%f\t%f\n", differenceLeft, differenceRight, speedLeft, speedRight);
-		if(differenceLeft == distanceTarget && differenceRight == distanceTarget)
+		printf("T\t%i\t%i\t%f\t%f\n", differenceLeft, differenceRight, speedLeft, speedRight);
+		if(differenceLeft == x && differenceRight == x)
 			break;
-		else if (differenceLeft > distanceTarget && differenceRight > distanceTarget)
+		else if (differenceLeft > x && differenceRight > x)
 			set_motors(-(int)speedLeft, -(int)speedRight);
 		else
 			set_motors((int)speedLeft, (int)speedRight);
@@ -125,7 +96,7 @@ int main()
 	initialize_robot();
 	int i = 0;
 	float angle;
-	float distance;
+	int distance;
 	float speed1;
 	float speed2;
 	char direction;
@@ -141,13 +112,11 @@ int main()
 	printf("Enter the angle\n");
 	scanf("%f", &angle);
 	*/
-
-	distance = 1;
+	distance = 500;
 	speed1 = 127;
 	speed2 = 127;
 	direction = 'L';
 	angle = 90;
-	turn(direction, 180, speed2);
 	while(i < 4)
 	{
 		straightLine(distance, speed1);
