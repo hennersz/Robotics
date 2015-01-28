6350b534fbe3209e2c704e19afa58243ca056b02
Axel Goetz <axel.goez@hotmail.com>
2015-01-28 21:25:35 +0000

    coordinates
    Did some testing with coordinates

--- Task2/Task2.2/Task2.2.c
+++ Task2/Task2.2/Task2.2.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <stdbool.h>
 #include <math.h>
 #include "picomms.h"
@@ -115,21 +112,48 @@ int calculateMotorValue(int *frontLeft, int *frontRight, int *backLeft, int *bac
 	else 
 		return 0;
 }
+//need to fix angle
+int findAngle(int *leftEncoder, int *rightEncoder)
+{
+	float ratio = 0.4286;
+	float temp;
+	if(*leftEncoder >= *rightEncoder)
+		temp = (float)(*leftEncoder - *rightEncoder) * ratio; 
+	else
+		temp = (float)(*rightEncoder - *leftEncoder + 90) * ratio;
+	return (int)temp % 360;
+}
 
 
+void distanceTravelled(int *leftEncoder, int *rightEncoder, float *x, float *y)
+{
+	int previousLeft = *leftEncoder;
+	int previousRight = *rightEncoder;
+	get_motor_encoders(leftEncoder, rightEncoder);
+	int angle = findAngle(leftEncoder, rightEncoder);
+	float distance = (float)((*leftEncoder - previousLeft) + (*rightEncoder - previousRight)) / 2;
+	printf("Angle = %i\tDistance = %f\n", angle, distance);
+	*x += distance * (sin(angle));
+	*y += distance * (cos(angle));
+	printf("x = %f\t y = %f\n", *x, *y);
+}  
 
 void wallFollower(int speed)
 {
 	int frontleft, frontright; 
 	int sideleft, sideright; 
 	int leftBumper,rightBumper;
+	int leftEncoder, rightEncoder;
+	float x= 0, y = 0;
 	int total = 0;
 	set_ir_angle(1, -45);
+	get_motor_encoders(&leftEncoder, &rightEncoder);
 	while(1)
 	{
 		total = calculateMotorValue(&frontleft, &frontright, &sideleft, &sideright, total, speed);
 		checkWalls(&frontleft, &frontright,&sideleft, &sideright);
 		stopped(&leftBumper, &rightBumper);
+		distanceTravelled(&leftEncoder, &rightEncoder, &x, &y);
 	}
 }
 
