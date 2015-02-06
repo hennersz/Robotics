3785a00b096fa1d03fd7c53f7765a533464298e5
Axel Goetz <axel.goez@hotmail.com>
2015-02-06 16:41:51 +0000

    wall follower

--- Task3/Task3.1/Task3.1.c
+++ Task3/Task3.1/Task3.1.c
@@ -5,51 +5,10 @@
 #include <math.h>
 #include "picomms.h"
 #include "mapping.h"
-#define TARGETDISTANCE 20
+#define TARGETDISTANCE 34
 #define MAXSPEED 127
-float ratio = 0.21;
-
-/*void calculateRatio(float wheelDiam, float robotDiam)
-{
-	float wheelCirc = wheelDiam * M_PI;
-	float robotCirc = robotDiam * M_PI;
-	ratio = 1/(robotCirc/wheelCirc);
-}
-
-int findAngle(int *leftEncoder, int *rightEncoder)
-{
-	float temp;
-	temp = (float)(*leftEncoder - *rightEncoder)/2.0 * ratio;
-	return (int)temp % 360;
-}
-
-double toRadians(double angle)
-{
-	return (double)(angle * (M_PI/180));
-}
->>>>>>> FETCH_HEAD
-
-#define TARGETDISTANCE 35
 #define STOPPINGDISTANCE 13
-#define MAXSPEED 127
 
-<<<<<<< HEAD
-=======
-void distanceTravelled(int *leftEncoder, int *rightEncoder, float *x, float *y)
-{
-	int previousLeft = *leftEncoder;
-	int previousRight = *rightEncoder;
-	get_motor_encoders(leftEncoder, rightEncoder);
-	int angle = findAngle(leftEncoder, rightEncoder);
-	float distance = (float)((*leftEncoder - previousLeft) + (*rightEncoder - previousRight)) / 2;
-	//printf("Angle = %i\tDistance = %f\n", angle, distance);
-	double radians = toRadians((double)angle);
-	//printf("Radians = %f\n", radians);
-	*y += (distance * (cos(radians))) / 12;
-	*x += (distance * (sin(radians))) / 12;
-	printf("x = %f\t y = %f\n", *x, *y);
-} 
-*/
 int proportional(int *frontLeft)		//calculate proportional value of how far the robot is from the wall
 {
 	if(*frontLeft > 50)
@@ -91,7 +50,7 @@ int calculateMotorValue(int *frontLeft, int *frontRight, int *integralValue, int
 	*integralValue +=proportionalValue;
 	if (*integralValue > 100 && *integralValue < -100)   //Limit the impact of integral value
 		*integralValue = 0;
-	float finalValue = proportionalValue * 8 + differentialValue * 30 + *integralValue * 0.1;
+	float finalValue = proportionalValue * (speed/10) + differentialValue * 30 + *integralValue * 0.1;
 	int finalSpeed = speed + finalValue;
 
 	if(finalSpeed > MAXSPEED)		//filters high or low speeds
@@ -104,30 +63,10 @@ int calculateMotorValue(int *frontLeft, int *frontRight, int *integralValue, int
 void checkWalls(int frontLeft, int frontRight, int speed, int finalSpeed)
 {
 
-	/*int leftEncoder, rightEncoder;
-	get_motor_encoders(&leftEncoder, &rightEncoder);
-	*/
-	if(frontRight < 20)
+	if(frontRight < 30)
 		set_motors(frontRight, frontRight);
 	else
 		set_motors(speed, finalSpeed);
-		/*while((*frontLeft < 38 && *frontRight < 38) || *frontRight < 15)
-        {
-            set_motors(10,-10);
-            get_front_ir_dists(frontLeft, frontRight);
-            //get_side_ir_dists(sideLeft, sideRight);
-            //distanceTravelled(&leftEncoder, &rightEncoder, x, y);
-        }
-    else
-        while((*frontLeft < 38 && *frontRight < 38) || *frontRight < 15)
-        {
-            set_motors(-10,10);
-            get_front_ir_dists(frontLeft, frontRight);
-            //get_side_ir_dists(sideLeft, sideRight);
-            //distanceTravelled(&leftEncoder, &rightEncoder, x, y);
-        }
-	}
-	*/
 }
 
 void wallFollower(int speed)
@@ -157,8 +96,7 @@ int main()
 {
 	connect_to_robot();
 	initialize_robot();
-	//calculateRatio(96.0, 260.0);
-	int leftEncoder, rightEncoder;
-	get_motor_encoders(&leftEncoder, &rightEncoder);
+	
 	wallFollower(50);
+	return 0;
 }
